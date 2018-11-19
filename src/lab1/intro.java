900577637b374ae71564487b9fd56b81366aaf04
Aunchalika <Aunchalika@gmail.com>
2018-11-19 16:34:36 +0700

    Lab 1 commit

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -1,4 +1,26 @@
 package lab1;
 
 public class intro {
-}
+    //to do code here
+    public static void main(String[] args) {
+        System.out.println("Aunchalika Srimalanon");
+        System.out.println("is441");
+        System.out.println("358411110043");
+        System.out.println("0848518434");
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}//class
