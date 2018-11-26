1a22e2e96f6c8ac2c4821589f02aad8bdd4aa6f4
Aunchalika <Aunchalika@gmail.com>
2018-11-26 16:06:02 +0700

    Lab 2 ex1

---
+++ src/lad2/ex1.java
@@ -0,0 +1,36 @@
+package lad2;
+
+public class ex1 {
+    public static void main(String[]args){
+        //integre
+        int a;
+        a=10;
+        System.out.println(a);
+        int b=20;
+        System.out.println(b);
+        int c=a+b;
+        System.out.println("ผลรวมของ a และ b เท่ากับ\t"+c);
+
+        //real namber
+
+
+        double q=5.5;
+        q = q*10;
+        System.out.println(q);
+
+        //character
+        char d = '#';
+        System.out.println(d);
+
+        //string
+        String str ="Aunchalika Srimalanon";
+        System.out.println("My name is\t"+str);
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+
+
+
+
+    }//main
+
+}//clss
