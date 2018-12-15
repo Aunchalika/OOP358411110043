64d6b907a2bf421f84700d452e4ee4b8015474ed
Aunchalika <Aunchalika@gmail.com>
2018-12-15 15:06:21 +0700

    Lab 2 loop ex4-7

---
+++ src/lad2/ex4.java
@@ -0,0 +1,21 @@
+package lad2;
+
+public class ex4 {
+    public static void main(String[]args){
+        // print "hello" 10 time
+        for (int count =1; count <=15; count++) {
+
+            //statements
+            System.out.print (" hello ");
+            if (count %2==0)
+                System.out.println("*");
+            else
+                System.out.println();
+
+        }
+
+
+
+
+    }// main
+}
---
+++ src/lad2/ex7.java
@@ -0,0 +1,16 @@
+package lad2;
+
+public class ex7 {
+    public static void main(String[]args){
+        int n= 1;
+        do {
+            System.out.println("hello."+n);
+        }while ( n++ <= 10);
+
+
+
+
+    }
+}//class
+
+
