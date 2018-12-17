b90d83794b71f5911b749a690c5e8e0ecdda23c4
Aunchalika <Aunchalika@gmail.com>
2018-12-17 15:15:48 +0700

    Lab  ex3

---
+++ src/lab4/ex3.java
@@ -0,0 +1,20 @@
+package lab4;
+
+public class ex3 {
+
+    public static void  main (String []args ) {
+        A( 5.5, 5);
+    }
+
+    protected static int A(int a,int b) {
+        return a * b;
+    }
+    protected static double A(double a,double b) {
+        return a * b;
+    }
+
+
+    }
+
+
+
